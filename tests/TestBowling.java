175003e94c7a7093390bb1b57464664492c979a6
somename <someemail>
2017-06-09 14:50:18 +0200

    besouro automatic message

--- tests/TestBowling.java
+++ tests/TestBowling.java
@@ -24,6 +24,15 @@ public class TestBowling {
 		game.addFrame(new Frame(0,0));
 		game.addFrame(new Frame(0,0));
 		game.addFrame(new Frame(0,0));
+	}
+	
+	
+	@Test
+	public void testIfFrameSpareWorks() throws BowlingException{
+		BowlingGame game=new BowlingGame();
+		game.addFrame(new Frame(5,5));
+		String check=Boolean.toString(game.getFrames().get(0).isSpare());
+		assertEquals("Greska",check);
 		
 		
 	}
